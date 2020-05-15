00599b1a8a9e663fc5bc09dad3b27dcf5bc41ca5
macaronjung <macaronjung@gmail.com>
2020-05-15 14:42:26 +0900

    드림코딩_JS_입문_5편(function&return)

--- 05_function/function.js
+++ 05_function/function.js
@@ -91,3 +91,98 @@ function sum(a, b) {
 }
 const result = sum(1, 2);
 console.log(`sum : ${sum(1,2)}`);
+
+// 7. Early return, early exit
+
+// bad logic
+function upgradeUser(user) {
+  if(user.point > 10) {
+    // long upgrade logic...
+    // 블럭안에서 로직을 많이 작성하면 가독성이 나뻐짐
+  }
+}
+
+// good logic
+function upgradeUser(user) {
+  if (user.point <= 10) {
+    return; // 값이 아닐때는 빨리 종료 후 리턴값 반환
+  }
+  // long upgrade logic...
+  // 조건이 맞을때만 긴 로직을 실행
+} 
+// 8. Function expression
+
+// First-class function
+// 함수는 변수와 마찬가지
+// functions are treated like any other variable
+// 변수로 지정가능
+// can be assigned as a value to variable
+// 파라미터로 전달가능
+// can be passed as an argument to other function
+// 리턴값으로 전달가능
+// can be returned by another function
+
+// 1. Function expression
+// 2. function declaration can be called earlier than it is defiend. (hoisted)
+// function expression -> 할당된 다음부터 호출이 가능
+// 3. function expression is created when the execution reached it.
+
+// 함수 선언과 동시에 변수에 할당
+const print = function () { // anonymous function
+  console.log("print");
+};
+print(); // 변수에 할당된 함수를 호출
+
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(`sumAgain : ${sumAgain(1, 5)}`);
+
+// 9. Callback function using function expression
+// 함수를 전달하여 상황에 맞으면(니가 원하면) 전달된 함수를 불러 -> callback function
+// 두가지의 callback function이 parameter로 전달되었음
+function randomQuiz(answer, printYes, printNo) {
+  if (answer === "love you") {
+    printYes();
+  } else {
+    printNo();
+  }
+}
+
+// anonymous function
+const printYes = function () {
+  console.log("Yes!!!");
+};
+// named function
+// better debugging in debugger's stack traces
+// recursions -> 재귀
+const printNo = function print () {
+  console.log("No!!!");
+  // print(); -> 계속해서 호출 숫자가 늘어남 / 피보나치 계산, 반복 평균값 계산등에서만 제한적 사용
+};
+randomQuiz("wrong", printYes, printNo);
+randomQuiz("love you", printYes, printNo);
+
+// 10. Arrow function
+// always anonymous
+// 배열, 리스트등에서 활용도 높음
+const simplePrint = function () {
+  console.log("simplePrint!!!");
+};
+const simplePrint2 = () => console.log("arrow function!!!");
+
+const add = function (a, b) {
+  return a + b;
+};
+const add2 = (a, b) => a + b; 
+
+const simpleMultiply = (a, b) => {
+  // do something more
+  return a * b; // -> 블럭 사용시 리턴이 필수
+};
+
+// 11. IIFE : Immediately Invoked Function Expression
+// 함수를 선언함과 동시에 호출 실시
+(function hello() {
+  console.log("IIFE");
+})();
